97f98d7c9cb5f00d73c5652d916dc3550bfd4cca
Rohith <eswarrohith238@gmail.com>
2025-02-07 21:58:53 +0530

    initial script created successfully

---
+++ scriptlearning.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#Author:Rohith
+#date:"feb,7 2025"
+#description:"initial stage of learning"
+#Date modified:
+#Purpose of modification:
+Date
+PWD
+touch Raju{1..4}.txt
+echo "raju txt files created"
+ABC=10
+abc=12
+echo "$abc"
+echo "$ABC"
+XYZ=$ABC
+echo "$XYZ"
+echo "$0"
+echo "$1"
+echo "$2"
+echo "$4"
+echo "$@"
+echo "$!"
+echo "$$"
+echo "end of script"
